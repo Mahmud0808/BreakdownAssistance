380cefc34a71a6a3e962d8f7e62f32f72b9731f8
DrDisagree <crazymahmud08@gmail.com>
2025-01-03 20:25:40 +0600

    Add SSLCommmerz payment gateway API

--- app/src/main/java/com/habibur/breakdown_assistance/config/Constants.java
+++ app/src/main/java/com/habibur/breakdown_assistance/config/Constants.java
@@ -7,4 +7,8 @@ public class Constants {
     public static final String SERVICE_DATABASE = "service_list";
     public static final String GARAGES_DATABASE = "garage_list";
     public static final String COMPLAINTS_DATABASE = "complaints_list";
+
+    // SSLCommerz
+    public static final String SSLCOMMERZ_STORE_ID = "YOUR_STORE_ID";
+    public static final String SSLCOMMERZ_STORE_PASSWORD = "YOUR_STORE_PASSWORD";
 }
