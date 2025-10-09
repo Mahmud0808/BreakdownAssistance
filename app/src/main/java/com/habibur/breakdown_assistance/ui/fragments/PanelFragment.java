66ec1d52218aa21435d62607a7c059d64ab8220e
DrDisagree <29881338+Mahmud0808@users.noreply.github.com>
2025-10-09 13:24:19 +0600

    Redesign mechanic/admin panel

--- app/src/main/java/com/habibur/breakdown_assistance/ui/fragments/PanelFragment.java
+++ app/src/main/java/com/habibur/breakdown_assistance/ui/fragments/PanelFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 
 import com.habibur.breakdown_assistance.R;
+import com.habibur.breakdown_assistance.config.Prefs;
 import com.habibur.breakdown_assistance.databinding.FragmentPanelBinding;
 import com.habibur.breakdown_assistance.utils.ViewUtils;
 
@@ -21,9 +22,19 @@ public class PanelFragment extends BaseFragment {
 
         ViewUtils.setToolbarTitle(requireContext(), binding.header.toolbar, R.string.panel, true);
 
+        binding.cardAddService.setVisibility(Prefs.isAdmin() ? View.VISIBLE : View.GONE);
         binding.cardAddGarage.setOnClickListener(v -> MainFragment.replaceFragment(new NewGarageFragment()));
 
+        binding.cardAddService.setVisibility(Prefs.isAdmin() ? View.VISIBLE : View.GONE);
         binding.cardAddService.setOnClickListener(v -> MainFragment.replaceFragment(new NewServiceFragment()));
+
+        binding.cardViewUsers.setVisibility(Prefs.isAdmin() ? View.VISIBLE : View.GONE);
+
+        binding.cardViewComplaints.setVisibility(Prefs.isAdmin() ? View.VISIBLE : View.GONE);
+
+        binding.cardServiceRequest.setVisibility(Prefs.isMechanic() ? View.VISIBLE : View.GONE);
+
+        binding.cardViewHistory.setVisibility(Prefs.isMechanic() ? View.VISIBLE : View.GONE);
 
         return binding.getRoot();
     }
